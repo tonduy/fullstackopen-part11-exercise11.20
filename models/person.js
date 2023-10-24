927a2ead2673d302b830aee4dd80bbc0947c7471
tonduy <80115135+tonduy@users.noreply.github.com>
2023-10-24 14:31:58 +0200

    Fullstack part11 exercise 11.20

---
+++ models/person.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+
+// eslint-disable-next-line no-undef
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const customNumberValidator = [
+  {
+    validator: (number) => {
+      const parts = number.split('-')
+
+      if (parts.length !== 2){
+        return false
+      }
+
+      if (!/^\d{2,3}-\d+$/.test(number)){
+        return false
+      }
+
+      return true
+    },
+    message: 'invalid phone format'
+  }
+]
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: customNumberValidator
+  },
+})
+
+
+
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
